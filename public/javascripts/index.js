c529e3c8e465afe7f5d66cf677fdeabfe99e0b8a
LamVu1 <lvu2548@gmail.com>
2019-10-01 13:47:42 -0700

    Created fetch requests for api

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,37 +1,24 @@
-const axios = require('axios');
+// const axios = require('axios');
+import test from './test';
+
+
+
+
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    let isbn = '0201558025';
-    axios.get(`/books/${isbn}`)
-    .then((response) => {
-        console.log(response); 
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
-
-    let query = "grace hopper";
-    axios.get(`/search?string=${query}`)
-    .then((response) => {
-        console.log(response);
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
-    
 })
 
-var requestURL = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-09-01&minmagnitude=5';
-var request = new XMLHttpRequest();
-request.open('GET', requestURL);
-request.responseType = 'json';
+// var requestURL = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-09-01&minmagnitude=5';
+// var request = new XMLHttpRequest();
+// request.open('GET', requestURL);
+// request.responseType = 'json';
 
-request.onload = function() {
-    let data=JSON.parse(this.response);
-    data.forEach(res=>{
-      console.log(res);
-    })
-}
+// request.onload = function() {
+//     let data=JSON.parse(this.response);
+//     data.forEach(res=>{
+//       console.log(res);
+//     })
+// }
 
-request.send();
+// request.send();
